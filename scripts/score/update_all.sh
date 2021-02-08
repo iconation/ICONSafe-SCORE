f961f64cc09f37d8c04c8dbae5474dc122296f7b
ICONationTeam <iconation.team@gmail.com>
2021-02-09 00:21:33 +0100

    Remove set_registrar from update_all.sh

--- scripts/score/update_all.sh
+++ scripts/score/update_all.sh
@@ -9,13 +9,6 @@ function print_usage {
     exit 1
 }
 
-function set_registrar {
-    package=$1
-    registrar=$(cat ./config/address_registrar/${network}/score_address.txt)
-    cli_config=$(cat ./config/${package}/${network}/tbears_cli_config.json | jq '.deploy.scoreParams.registrar_address = "'${registrar}'"')
-    echo -ne "$cli_config" > ./config/${package}/${network}/tbears_cli_config.json
-}
-
 function process {
 
     if [[ ("$network" == "") ]]; then
