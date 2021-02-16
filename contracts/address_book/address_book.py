770bd73f83e77db7b2e948b307ada2cf75c4dd27
ICONationTeam <iconation.team@gmail.com>
2021-02-16 19:41:31 +0100

    Add address book

--- contracts/address_book/address_book.py
+++ contracts/address_book/address_book.py
@@ -18,25 +18,25 @@ from iconservice import *
 from ..scorelib.exception import *
 from ..scorelib.maintenance import *
 from ..scorelib.version import *
-from ..scorelib.auth import *
 from ..scorelib.set import *
 from ..scorelib.iterable_dict import *
 from ..scorelib.consts import *
 
-from ..interfaces.address_registrar import *
+from ..interfaces.event_manager import *
+from ..interfaces.address_book import *
+from ..domain.domain import *
 
 from .consts import *
 
-class SenderIsNotOwner(Exception):
-    pass
 
-
-class AddressRegistrar(
+class AddressBook(
     IconScoreBase,
-    ABCAddressRegistrar,
+    ABCAddressBook,
     IconScoreMaintenance,
     IconScoreVersion,
     IconScoreExceptionHandler,
+
+    EventManagerProxy
 ):
     _NAME = "ADDRESS_REGISTRAR"
 
@@ -49,21 +49,18 @@ class AddressRegistrar(
     # ================================================
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
-        self._owners = SetDB(f"{AddressRegistrar._NAME}_owners", self.db, Address)
-        self._address_register = IterableDictDB(f"{AddressRegistrar._NAME}_address_register", self.db, Address, str)
-        self._name_register = IterableDictDB(f"{AddressRegistrar._NAME}_name_register", self.db, str, Address)
+        self._address_register = IterableDictDB(f"{AddressBook._NAME}_address_register", self.db, Address, str)
+        self._name_register = IterableDictDB(f"{AddressBook._NAME}_name_register", self.db, str, Address)
 
     @catch_exception
-    def on_install(self) -> None:
+    def on_install(self, registrar_address: Address) -> None:
         super().on_install()
+        self.set_registrar_address(registrar_address)
         self.maintenance_disable()
         self.version_update(VERSION)
-        
-        self.add_owner(self.msg.sender)
-        self.register(AddressRegistrarProxy.NAME, self.address)
 
     @catch_exception
-    def on_update(self) -> None:
+    def on_update(self, registrar_address: Address) -> None:
         super().on_update()
 
         # if self.is_less_than_target_version('1.0.0'):
@@ -74,36 +71,36 @@ class AddressRegistrar(
     # ================================================
     #  Private methods
     # ================================================
-    def __check_owner(self, address: Address) -> None:
-        if not address in self._owners:
-            raise SenderIsNotOwner(AddressRegistrar._NAME)
 
     # ================================================
     #  External methods
     # ================================================
     @external
     @catch_exception
-    def register(self, name: str, address: Address) -> None:
-        self.__check_owner(self.msg.sender)
+    @only_iconsafe
+    def book_register(self, name: str, address: Address) -> None:
         self._address_register[name] = address
         self._name_register[address] = name
 
     @external
     @catch_exception
-    def unregister(self, name: str) -> None:
-        self.__check_owner(self.msg.sender)
+    @only_iconsafe
+    def book_unregister(self, name: str) -> None:
         address = self._address_register[name]
         del self._name_register[address]
         del self._address_register[name]
 
+    # ================================================
+    #  ReadOnly External methods
+    # ================================================
     @external(readonly=True)
     @catch_exception
-    def resolve(self, name: str) -> Address:
+    def book_resolve(self, name: str) -> Address:
         return self._address_register[name]
 
     @external(readonly=True)
     @catch_exception
-    def resolve_many(self, names: List[str]) -> List[Address]:
+    def book_resolve_many(self, names: List[str]) -> List[Address]:
         result = []
         for name in names:
             result.append(self._address_register[name])
@@ -111,31 +108,13 @@ class AddressRegistrar(
 
     @external(readonly=True)
     @catch_exception
-    def reverse_resolve(self, address: Address) -> str:
+    def book_reverse_resolve(self, address: Address) -> str:
         return self._name_register[address]
 
     @external(readonly=True)
     @catch_exception
-    def reverse_resolve_many(self, addresses: List[Address]) -> List[str]:
+    def book_reverse_resolve_many(self, addresses: List[Address]) -> List[str]:
         result = []
         for address in addresses[0:MAX_ITERATION_LOOP]:
             result.append(self._name_register[address])
         return result
-
-    # --- Owners management ---
-    @external
-    @catch_exception
-    @only_owner
-    def add_owner(self, address: Address) -> None:
-        self._owners.add(address)
-
-    @external
-    @catch_exception
-    @only_owner
-    def remove_owner(self, address: Address) -> None:
-        self._owners.remove(address)
-
-    @external(readonly=True)
-    @catch_exception
-    def get_owners(self, offset: int = 0) -> list:
-        return self._owners.select(offset)
---
+++ contracts/interfaces/address_book.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2021 ICONation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from iconservice import *
+from .address_registrar import *
+from ..utility.proxy_score import *
+
+class ABCAddressBook(ABC):
+
+    @abstractmethod
+    def book_resolve(self, name: str) -> Address:
+        pass
+
+    @abstractmethod
+    def book_reverse_resolve(self, address: Address) -> str:
+        pass
+
+    @abstractmethod
+    def book_resolve_many(self, names: List[str]) -> List[Address]:
+        pass
+
+    @abstractmethod
+    def book_reverse_resolve_many(self, addresses: List[Address]) -> List[str]:
+        pass
+
+    @abstractmethod
+    def book_register(self, name: str, address: Address) -> None:
+        pass
+
+    @abstractmethod
+    def book_unregister(self, name: str) -> None:
+        pass
+
+class ABCAddressBookSystemLevel(ABCAddressBook):
+    pass
+
+class AddressBookProxy(AddressRegistrarProxy):
+
+    NAME = "ADDRESS_BOOK_PROXY"
+    AddressBookInterface = ProxyScore(ABCAddressBookSystemLevel)
+
+    # ================================================
+    #  Fields
+    # ================================================
+    @property
+    def address_book(self):
+        address = self.registrar.resolve(AddressBookProxy.NAME)
+        if not address:
+            raise AddressNotInRegistrar(AddressBookProxy.NAME)
+
+        return self.create_interface_score(address, AddressBookProxy.AddressBookInterface)
