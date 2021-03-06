c789dc02e52a5255c82cec821a5bf0adbd7aca7e
ICONationTeam <iconation.team@gmail.com>
2021-03-06 18:51:57 +0100

    Add package support to submit_transaction.sh

--- scripts/score/submit_transaction.sh
+++ scripts/score/submit_transaction.sh
@@ -6,15 +6,20 @@ function print_usage {
     usage_header ${0}
     usage_option " -n <network> : Network to use (localhost, yeouido, euljiro or mainnet)"
     usage_option " -s <sub_transactions.json> : Path to sub transactions json file"
+    usage_option " -p <package> : package name"
     usage_footer
     exit 1
 }
 
 function process {
 
-    if [[ ("$network" == "") || ("$subtx" == "") ]]; then
+    if [[ ("$network" == "") || ("$subtx" == "") || ("$package" == "") ]]; then
         print_usage
     fi
+
+    cli_config=$(cat ./config/${package}/${network}/tbears_cli_config.json)
+    cli_config=${cli_config//PACKAGE_NAME/${package}}
+    echo $cli_config >"${cli_config_file:=$(mktemp)}"
 
     command=$(cat <<-COMMAND
     tbears sendtx <(
@@ -22,22 +27,25 @@ function process {
             ${network@Q}
             ${subtx@Q}
         )
-        -c ./config/iconsafe/${network}/tbears_cli_config.json
+        -c ${cli_config_file}
 COMMAND
 )
 
-    txresult=$(./scripts/icon/txresult.sh -n "${network}" -c "${command}")
+    txresult=$(./scripts/icon/txresult.sh -n "${network}" -p "${package}" -c "${command}")
     echo -e "${txresult}"
 }
 
 # Parameters
-while getopts "n:s:" option; do
+while getopts "n:s:p:" option; do
     case "${option}" in
         n)
             network=${OPTARG}
             ;;
         s)
             subtx=${OPTARG}
+            ;;
+        p)
+            package=${OPTARG}
             ;;
         *)
             print_usage 
