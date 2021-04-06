cecc4cedffe0927d5e8f8ce7c38c64467300148c
ICONationTeam <iconation.team@gmail.com>
2021-04-06 19:07:36 +0200

    Fix wallet_settings_manager deploy JSON

--- scripts/score/deploy_all.sh
+++ scripts/score/deploy_all.sh
@@ -46,13 +46,14 @@ function process {
     ./scripts/score/deploy_score.sh -n ${network} -p event_manager
     echo "Deploying transaction_manager..."
     ./scripts/score/deploy_score.sh -n ${network} -p transaction_manager
-    echo "Deploying wallet_owners_manager..."
-    operator=$(cat ./config/keystores/${network}/operator.icx | jq .address)
-    cli_config=$(cat ./config/wallet_owners_manager/${network}/tbears_cli_config.json | jq '.deploy.scoreParams.owners[0].address = '${operator}'')
-    echo -ne "$cli_config" > ./config/wallet_owners_manager/${network}/tbears_cli_config.json
-    ./scripts/score/deploy_score.sh -n ${network} -p wallet_owners_manager
     echo "Deploying wallet_settings_manager..."
     ./scripts/score/deploy_score.sh -n ${network} -p wallet_settings_manager
+
+    echo "Deploying wallet_owners_manager..."
+    operator=$(cat ./config/keystores/${network}/operator.icx | jq .address)
+    wallet_owners_manager_config=$(cat ./config/wallet_owners_manager/${network}/tbears_cli_config.json | jq '.deploy.scoreParams.owners[0].address = '${operator}'')
+    echo -ne "$wallet_owners_manager_config" > ./config/wallet_owners_manager/${network}/tbears_cli_config.json
+    ./scripts/score/deploy_score.sh -n ${network} -p wallet_owners_manager
 }
 
 # Parameters
